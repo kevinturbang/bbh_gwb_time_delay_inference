ac8bec4fc49ba169bdc1d5588ba9f1aed111db25
Kevin Turbang <kevin.turbang@ldas-pcdev8.ligo.caltech.edu>
2023-10-09 07:48:30 -0700

    Making consistent with zenodo content

--- data/download_data_from_zenodo.sh
+++ data/download_data_from_zenodo.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
 # Download and unzip
-curl https://zenodo.org/record/8087858/files/autoregressive-bbh-inference-data.zip --output "bbh-gwb-time-delay-inference-data.zip"
-unzip bbh-gwb-time-delay-inference-data.zip
+curl https://sandbox.zenodo.org/record/1246333/files/bbh_time_delay_data.zip --output "bbh_time_delay_data.zip"
+unzip bbh_time_delay_data.zip
 
 # Move input data to ../input/
-mv sampleDict_FAR_1_in_1_yr.pickle ../input/
-mv injectionDict_FAR_1_in_1.pickle ../input/
-mv delayedRateDataMD.npy ../input/
-mv delayedRateDataVA.npy ../input/
-mv O3_GWB_measurement.mat ../input/
-mv O3_PI_curve.csv ../input/
-mv O5_PI_curve.csv ../input/
-mv Omega_measurement_detectable_O5_MD.mat ../input
-mv Omega_measurement_undetectable_O5_MD.mat ../input
-mv Omega_measurement_detectable_O5_VA.mat ../input
-mv Omega_measurement_undetectable_O5_VA.mat ../input
+mv bbh_time_delay_data/sampleDict_FAR_1_in_1_yr.pickle ../input/
+mv bbh_time_delay_data/injectionDict_FAR_1_in_1.pickle ../input/.
+mv bbh_time_delay_data/delayedRateDataMD.npy ../input/
+mv bbh_time_delay_data/delayedRateDataVA.npy ../input/
+mv bbh_time_delay_data/O3_GWB_measurement.mat ../input/
+mv bbh_time_delay_data/O3_PI_curve.csv ../input/
+mv bbh_time_delay_data/O5_PI_curve.csv ../input/
+mv bbh_time_delay_data/Omega_measurement_detectable_O5_MD.mat ../input/
+mv bbh_time_delay_data/Omega_measurement_undetectable_O5_MD.mat ../input/
+mv bbh_time_delay_data/Omega_measurement_detectable_O5_VA.mat ../input
+mv bbh_time_delay_data/Omega_measurement_undetectable_O5_VA.mat ../input/
+
+# Move input data to ../
+mv bbh_time_delay_data/O3_CBC_GWB_MD.nc .
+mv bbh_time_delay_data/O3_CBC_GWB_VA.nc .
+mv bbh_time_delay_data/O3_CBC_MD.nc .
+mv bbh_time_delay_data/O5_CBC_GWB_detectable_MD.nc .
+mv bbh_time_delay_data/O5_CBC_GWB_detectable_VA.nc .
+mv bbh_time_delay_data/O5_CBC_GWB_undetectable_MD.nc .
+mv bbh_time_delay_data/O5_CBC_GWB_undetectable_VA.nc .
 
 # Move input data to ../mock_gwb_O5/
-mv AplusDesign.txt ../mock_gwb_O5/
-mv freqs_detectable_O5.npy ../mock_gwb_O5/
-mv freqs_undetectable_O5.npy ../mock_gwb_O5/
-mv Omega_detectable_O5_MD.npy ../mock_gwb_O5/
-mv Omega_detectable_O5_VA.npy ../mock_gwb_O5/
-mv Omega_undetectable_O5_MD.npy ../mock_gwb_O5/
-mv Omega_undetectable_O5_VA.npy ../mock_gwb_O5/
-mv ORF_HL_freqs.npy ../mock_gwb_O5/
-mv ORF_HL.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/AplusDesign.txt ../mock_gwb_O5/
+mv bbh_time_delay_data/freqs_detectable_O5.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/freqs_undetectable_O5.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/Omega_detectable_O5_MD.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/Omega_detectable_O5_VA.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/Omega_undetectable_O5_MD.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/Omega_undetectable_O5_VA.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/ORF_HL_freqs.npy ../mock_gwb_O5/
+mv bbh_time_delay_data/ORF_HL.npy ../mock_gwb_O5/
 
-# Remove original zip files and annoying Mac OSX files
-rm bbh-gwb-time-delay-inference-data.zip
-rmdir __MACOSX/
+# Remove original zip files and directory
+rm bbh_time_delay_data.zip
+rm -r bbh_time_delay_data/
